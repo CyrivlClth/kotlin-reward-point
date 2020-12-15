4be14cba2cbedaa079d998bed8c09c1e23a5f02f
CyrivlClth <cyrivlclth@live.com>
2020-12-15 20:43:33 +0800

    feat: add point cannot be zero now.

--- src/main/kotlin/com/cyrivlclth/rewardpoint/domain/account/model/PointVO.kt
+++ src/main/kotlin/com/cyrivlclth/rewardpoint/domain/account/model/PointVO.kt
@@ -2,4 +2,8 @@ package com.cyrivlclth.rewardpoint.domain.account.model
 
 data class PointVO(
     val amount: Int,
-)
+) {
+    init {
+        require(amount != 0) { IllegalArgumentException("change amount cannot be zero") }
+    }
+}
